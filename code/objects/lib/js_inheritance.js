75b9dc6d6cfb852158dcc6ac149c84945189e728
Rob Levin <roblevintennis@gmail.com>
2009-10-28 22:25:00 -0700

    Finished: Parasitic Combination Inheritance pattern and Functional Inheritance pattern sections and implemented respective code/tests.

--- code/objects/lib/js_inheritance.js
+++ code/objects/lib/js_inheritance.js
@@ -2,7 +2,7 @@
 function Animal(name) {
     this.name = name;
     this.arr = [1,2,3];
-}
+};
 Animal.prototype = {
     constructor: Animal,
     whoAmI: function() { return "I am " + this.name + "!\n"; }
@@ -11,29 +11,70 @@ Animal.prototype = {
 function Dog(name, breed) {
     this.name = name;
     this.breed = breed;
-}
+};
 Dog.prototype = new Animal();
 Dog.prototype.getBreed = function() {
     return this.breed;
-}
+};
 Dog.prototype.bark = function() {
     return 'ruff ruff';
-}
+};
 
 // Combination Inheritance
 function Parent(name) {
     this.name = name;
     this.arr = [1,2,3];
-}
+};
 Parent.prototype = {
     constructor: Parent,
     toString: function() { return "My name is " + this.name; }
-}
+};
 function Child(name, age) {
-    this.age = age;
     Parent.call(this, name);
-}
+    this.age = age;
+};
+
+Child.prototype = new Parent();
+
 Child.prototype.getAge = function() {
     return this.age;
-}
+};
 
+// Prototypal Inheritance
+Object.prototype.inherit = function(p) {
+    NewObj = function(){};
+    NewObj.prototype = p;
+    return new NewObj(); 
+};
+
+// Paraphrasing of Nicholas Zakas's Prototype Inheritance helper
+function inheritPrototype(subType, superType) {
+    var prototype = Object.inherit(superType.prototype);
+    prototype.constructor = subType;
+    subType.prototype = prototype;
+};
+function SubType(name, age) {
+    Parent.call(this, name);
+    this.age = age;    
+};
+//Child.prototype = new Parent();   // Gets replaced by:
+inheritPrototype(SubType, Parent);  // Clobbers global object - would be better to namespace this!
+SubType.prototype.getAge = function() {
+    return this.age;
+};
+
+// Functional - Durable Pattern
+function super_func(blueprint) { 
+    var obj = {};
+    obj.getName = function() { return blueprint.name; };
+    obj.getAge  = function() { return blueprint.age; };
+    obj.getFoo  = function() { return blueprint.foo; };
+    obj.getBar  = function() { return blueprint.bar; };
+    return obj;
+};
+function sub_func(blueprint) {
+    blueprint.name = blueprint.name || "Crockford's Place";
+    supr = super_func(blueprint);
+    supr.coolAugment = function() { return "I give a fresh new perspective on things!" };
+    return supr;    
+};
--- code/objects/spec/spec.js_inheritance.js
+++ code/objects/spec/spec.js_inheritance.js
@@ -2,6 +2,9 @@ describe 'JavaScript Inheritance Tests'
     before_each
 	animal = new Animal("Onyx")
 	dog = new Dog("Sebastian", "Lab")
+	
+	person = { password : 'secret', toString : function(){ return '<Person>' } }
+	stub(person, 'toString').and_return('Original toString method!')	
     end
     describe 'Pseudoclassical Inheritance Creation'
 	it 'should create parent and child object using pseudoclassical inheritance'
@@ -52,8 +55,65 @@ describe 'JavaScript Inheritance Tests'
 	    child_1.arr.should.eql([1,2,3,999])
 	    child_2.arr.should.eql([1,2,3,333])
 	    child_1.getAge().should.eql 21
-	    child_1.constructor.should.eql Child
-	    child_1.should.be_a Child
+	    child_1.should.be_a Parent
 	end
     end
+
+    describe 'Prototypal Inheritance'
+	it 'should inherit properties from parent'
+	    person.toString().should.match /Original toString.*/i
+	    person.password.should.eql 'secret'
+	    joe = Object.inherit(person)
+	    joe.password.should.eql 'secret'
+	    joe.password = 'letmein'
+	    joe.password.should.eql 'letmein'
+	    person.password.should.eql 'secret'
+	end
+    end
+
+    describe 'Parisitic Combination Inheritance'
+	it 'should use inheritPrototype (to call parent constructor once) and still work as expected'
+	    sub = new SubType("Nicholas Zakas", 29)
+	    sub.toString().should.match /.*Nicholas Zakas/
+	    sub.getAge().should.eql 29
+	    charlie = new SubType("Charlie Brown", 69)
+	    charlie.arr.should.eql([1,2,3])
+	    charlie.arr.push(999)
+	    charlie.arr.should.eql([1,2,3,999])
+	    sub.arr.should.eql([1,2,3])	
+	    sub.should.be_an_instance_of SubType
+	    charlie.should.be_an_instance_of SubType
+	    (sub instanceof SubType).should.eql true 
+	    (sub instanceof Parent).should.eql true 
+	end
+    end
+    
+    describe 'Functional Durable Inheritance'
+	it 'should hide private variables'
+	    sup = new super_func( {name: "Superfly Douglas", age: 39, foo: "foo", bar: "bar"} )
+	    sup.getName().should.eql 'Superfly Douglas'
+	    sup.name.should.be_undefined
+	    sup.getAge().should.eql 39 
+	    sup.age.should.be_undefined
+	    sup.getFoo().should.eql 'foo'
+	    sup.foo.should.be_undefined
+	end
+	
+	it 'should create a descendent object that inherits properties while maintaining privacy'
+	    sub = new sub_func( {name: "Submarine", age: 1, foo: "food", bar: "barfly"} )
+	    sub.getName().should.eql 'Submarine'
+	    sub.name.should.be_undefined
+	    sub.getAge().should.eql 1 
+	    sub.age.should.be_undefined
+	    sub.getFoo().should.eql 'food'
+	    sub.foo.should.be_undefined 
+	    sub.getBar().should.eql 'barfly'
+	    sub.bar.should.be_undefined 
+	    sub.coolAugment().should.match /.*fresh new perspective.*/
+	    //sub.should.be_an_instance_of super_func NOPE!
+	    //sub.should.be_an_instance_of sub_func   NOPE!
+	    sub.should.be_an_instance_of Object 
+	end
+    end
+
 end
